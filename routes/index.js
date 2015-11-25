4e72a90e4c26f7e2fabc738fded3e8d576d428c8
Sophia Ngo <sophia.ngo@sjsu.edu>
2015-11-24 22:33:07 -0800

    used angular instead of ejs

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.sendFile('/Users/sophiango/Documents/NodeWorkspace/VWiC-2/views/index.html');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+  // res.render('index', { title: 'Express' });
 });
 
 router.get('/story', function(req, res, next) {
-  res.render('story');
+  // res.render('story');
+  res.sendFile(path.join(__dirname, '../views/story.html'));
 });
 
 
